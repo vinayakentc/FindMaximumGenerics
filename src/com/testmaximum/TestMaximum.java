cf5d8ace341b38eb9e9dde8bf04f4a7156692f67
vinayakentc <vinayakchavanentc@gmail.com>
2021-02-27 11:40:13 +0530

    Given 3 integers to find the maximum

--- src/com/testmaximum/TestMaximum.java
+++ src/com/testmaximum/TestMaximum.java
@@ -2,4 +2,12 @@ package com.testmaximum;
 
 public class TestMaximum {
 
+	public static Integer testMax(Integer a, Integer b, Integer c) {
+		Integer max = a;
+		if (b.compareTo(max) > 0)
+			max = b;
+		if (c.compareTo(max) > 0)
+			max = c;
+		return max;
+	}
 }
--- src/com/testmaximum/TestingMaximum.java
+++ src/com/testmaximum/TestingMaximum.java
@@ -1,5 +1,13 @@
 package com.testmaximum;
 
+
+import org.junit.Assert;
+import org.junit.Test;
+
 public class TestingMaximum {
 
+	@Test
+	public void testMaximumNumber() {
+		Assert.assertEquals((Integer) 7, TestMaximum.testMax(3, 7, 2));
+	}
 }
